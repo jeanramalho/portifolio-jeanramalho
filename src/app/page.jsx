79549198066939dce2a69d299e44bf20d727d652
jeanramalho <jeanramalho.dev@gmail.com>
2023-04-20 00:16:45 -0300

    importa componente na home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,11 @@
 import { Inter } from 'next/font/google'
+import Header from '../components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
     return ( 
-        <h1> Ola Mundo </h1>
+        <Header />
 
     )
 }
